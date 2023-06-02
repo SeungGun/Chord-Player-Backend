d0abccf5c18b248a4693abd7fa6552329cf4184a
SeungGun <saverrw2864@naver.com>
2023-06-02 15:16:26 +0900

    Initial Spring Project

---
+++ src/main/java/com/windry/chordplayer/ChordPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.windry.chordplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChordPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/windry/chordplayer/ChordPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windry.chordplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
