fd7891c401f1fbac81aeed7ca20f1d5a0178cf8e
SeungGun <saverrw2864@naver.com>
2023-06-04 15:00:31 +0900

    feat: 노래 생성을 위한 노래, 가사 DTO 클래스 정의

---
+++ src/main/java/com/windry/chordplayer/dto/LyricsDto.java
@@ -0,0 +1,33 @@
+package com.windry.chordplayer.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class LyricsDto {
+
+    private String lyrics;
+    private String tag;
+    private List<String> chords = new ArrayList<>();
+
+    @Builder
+    public LyricsDto(String lyrics, String tag, List<String> chords) {
+        this.lyrics = lyrics;
+        this.tag = tag;
+        this.chords = chords;
+    }
+
+    public void addAllChords(String ...chords){
+        this.chords.addAll(Arrays.asList(chords));
+    }
+
+    public static List<String> getAllChords(String ...chords){
+        return new ArrayList<>(Arrays.asList(chords));
+    }
+}
