32e597e90ea1c17ad3fa575d8f338b75db058273
SeungGun <saverrw2864@naver.com>
2023-06-09 17:31:04 +0900

    refactor: input 코드에 대한 예외 처리 추가 및 음계 시퀀스의 음수 값 현상으로 인한 순환 기능 추가

--- src/main/java/com/windry/chordplayer/dto/ChordUtil.java
+++ src/main/java/com/windry/chordplayer/dto/ChordUtil.java
@@ -1,5 +1,7 @@
 package com.windry.chordplayer.dto;
 
+import com.windry.chordplayer.exception.NotFoundChordException;
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -48,33 +50,68 @@ public class ChordUtil {
         return list;
     }
 
+    /**
+     * 주어진 변형 코드와 변경할 값만큼 키를 변경해주는 기능
+     *
+     * @param originChord 주어진 변형 코드
+     * @param amount      변경하고싶은 키 값 (음수, 양수 모두 가능)
+     * @return 최종 키가 변경된 코드 문자열
+     */
     public static String changeKey(String originChord, int amount) {
+        if (amount == 0)
+            return originChord;
+
+        if (originChord == null)
+            throw new NotFoundChordException();
+
+        // 분수 코드 존재할 경우
         if (originChord.contains("/")) {
             String[] split = originChord.split("/");
 
             String baseChord = extractBaseChord(split[0]);
             String slashRoot = split[1];
 
-            int order1 = chordOrdersAtoI.get(baseChord);
-            int order2 = chordOrdersAtoI.get(slashRoot);
+            int baseOrder = chordOrdersAtoI.get(baseChord);
+            int slashOrder = chordOrdersAtoI.get(slashRoot);
 
-            String s = chordOrdersItoA.get((order1 + amount) % 12)[0];
-            String s1 = chordOrdersItoA.get((order2 + amount) % 12)[0];
-            return split[0].replace(baseChord, s) + "/" + s1;
+            String changedBase = chordOrdersItoA.get(getCircularOrder(baseOrder, amount))[0];
+            String changedSlash = chordOrdersItoA.get(getCircularOrder(slashOrder, amount))[0];
+            return split[0].replace(baseChord, changedBase) + "/" + changedSlash;
         }
 
         String base = extractBaseChord(originChord);
         int order = chordOrdersAtoI.get(base);
 
-        return originChord.replace(base, chordOrdersItoA.get((order + amount) % 12)[0]);
+        return originChord.replace(base, chordOrdersItoA.get(getCircularOrder(order, amount))[0]);
     }
 
+    /**
+     * 주어진 코드를 정규표현식에 따라 매칭하고 원본 코드를 추출
+     *
+     * @param chord 주어진 변형 코드
+     * @return 변형 코드에서 추출한 원본 코드
+     * @throws NotFoundChordException: 주어진 코드에서 원본 코드를 추출할 수 없을 때 발생
+     */
     public static String extractBaseChord(String chord) {
         Matcher matcher = CHORD_PATTERN.matcher(chord);
         if (matcher.matches()) {
             return matcher.group(1);
         }
-        return null;
+        throw new NotFoundChordException();
     }
 
+    /**
+     * 주어진 음계 시퀀스의 순서를 키 변경에 따라 순환에 의한 순서를 반환해주는 기능
+     *
+     * @param order  주어진 순서
+     * @param amount 바꾸고자 하는 키 값
+     * @return 순환에 따라 변경된 순서 값 반환
+     */
+    private static int getCircularOrder(int order, int amount) {
+        if (order + amount < 0) {
+            return chordOrdersItoA.size() + (order + amount);
+        } else {
+            return (order + amount) % chordOrdersItoA.size();
+        }
+    }
 }
