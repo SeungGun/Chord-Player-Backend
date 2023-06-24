ce09e9955540c3e7b70297f4922d79e635457a71
SeungGun <saverrw2864@naver.com>
2023-06-24 13:54:35 +0900

    test: 기존 노래 테스트에 장르 엔티티 연관관계 추가

--- src/test/java/com/windry/chordplayer/service/SongServiceTest.java
+++ src/test/java/com/windry/chordplayer/service/SongServiceTest.java
@@ -1,11 +1,14 @@
 package com.windry.chordplayer.service;
 
 import com.windry.chordplayer.domain.Gender;
+import com.windry.chordplayer.domain.Genre;
 import com.windry.chordplayer.domain.Song;
+import com.windry.chordplayer.domain.SongGenre;
 import com.windry.chordplayer.dto.CreateSongDto;
 import com.windry.chordplayer.dto.LyricsDto;
 import com.windry.chordplayer.exception.DuplicateTitleAndArtistException;
 import com.windry.chordplayer.repository.ChordsRepository;
+import com.windry.chordplayer.repository.GenreRepository;
 import com.windry.chordplayer.repository.LyricsRepository;
 import com.windry.chordplayer.repository.SongRepository;
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -30,14 +34,19 @@ class SongServiceTest {
     @Autowired
     private ChordsRepository chordsRepository;
     @Autowired
+    private GenreRepository genreRepository;
+    @Autowired
     private SongService songService;
-
 
     @DisplayName("노래 데이터를 생성할 때, 중복되는 제목과 가수가 있을 경우 예외를 발생한다.")
     @Test
     void duplicateSongTitleAndArtistException() {
         // given
+        Genre genre = Genre.builder().name("락").build();
+        genreRepository.save(genre);
+
         Song song = new Song();
+
         CreateSongDto songDto = CreateSongDto.builder()
                 .title("하늘을 달리다")
                 .artist("이적")
@@ -47,6 +56,9 @@ class SongServiceTest {
                 .modulation(null)
                 .contents(null)
                 .build();
+
+        SongGenre songGenre = SongGenre.builder().genre(genre).song(song).build();
+        song.addGenre(songGenre);
 
         song.changeRequestFields(
                 songDto.getTitle(),
@@ -58,6 +70,8 @@ class SongServiceTest {
                 null);
 
         Song differentArtistSong = new Song();
+
+        differentArtistSong.addGenre(songGenre);
         differentArtistSong.changeRequestFields(
                 songDto.getTitle(),
                 "허각",
@@ -73,17 +87,25 @@ class SongServiceTest {
 
         // then
         Assertions.assertEquals("허각", songRepository.findById(2L).get().getArtist());
+        Assertions.assertEquals(1, songRepository.findById(2L).get().getSongGenres().size());
         Assertions.assertThrows(DuplicateTitleAndArtistException.class, () -> songService.validateDupSongAndArtist("하늘을달리다", "이적"));
     }
 
     @DisplayName("4박자 마디의 가사와 코드가 포함된 새로운 노래 데이터를 생성한다.")
     @Test
+    @Rollback(value = false)
     void createNewSong() {
         // given
+        Genre genre = Genre.builder().name("락").build();
+        genreRepository.save(genre);
+
         Song song = new Song();
         song.changeRequestFields(
                 "하늘을 달리다", "이적", "E", Gender.MALE, 116, null, null
         );
+
+        List<String> genreList = new ArrayList<>();
+        genreList.add("락");
 
         CreateSongDto songDto = CreateSongDto.builder()
                 .title("하늘을 달리다")
@@ -93,6 +115,7 @@ class SongServiceTest {
                 .gender(Gender.MALE)
                 .modulation(null)
                 .contents(null)
+                .genres(genreList)
                 .build();
 
         List<LyricsDto> lyricsDtoList = new ArrayList<>();
@@ -122,10 +145,11 @@ class SongServiceTest {
         // when
         Long newSong = songService.createNewSong(songDto);
 
+        Song song1 = songRepository.findById(newSong).get();
         // then
         org.assertj.core.api.Assertions.assertThat(song)
                 .usingRecursiveComparison()
-                .ignoringFields("createdDate", "modifiedDate", "id", "lyricsList")
-                .isEqualTo(songRepository.findById(newSong).get());
+                .ignoringFields("createdDate", "modifiedDate", "id", "lyricsList", "songGenres")
+                .isEqualTo(song1);
     }
 }
