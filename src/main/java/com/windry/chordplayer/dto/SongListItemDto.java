262b881f61c18de48bb51ec0ab2233a22b868afa
SeungGun <saverrw2864@naver.com>
2023-06-24 13:52:30 +0900

    feat: 노래 목록 조회 API 동적 쿼리를 통해 구현 완료

---
+++ src/main/java/com/windry/chordplayer/dto/SongListItemDto.java
@@ -0,0 +1,35 @@
+package com.windry.chordplayer.dto;
+
+import com.windry.chordplayer.domain.Gender;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class SongListItemDto {
+    private Long songId;
+    private String title;
+    private String artist;
+    private String originalKey;
+    private int bpm;
+    private String modulation;
+    private String note;
+    private Gender gender;
+    private List<String> genres;
+
+    @Builder
+    public SongListItemDto(Long songId, String title, String artist, String originalKey, int bpm, String modulation, String note, Gender gender, List<String> genres) {
+        this.songId = songId;
+        this.title = title;
+        this.artist = artist;
+        this.originalKey = originalKey;
+        this.bpm = bpm;
+        this.modulation = modulation;
+        this.note = note;
+        this.gender = gender;
+        this.genres = genres;
+    }
+}
