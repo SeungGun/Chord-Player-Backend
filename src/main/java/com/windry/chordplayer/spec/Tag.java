ee563a65b01d5a763d6223da72ae62baeef1cece
SeungGun <saverrw2864@naver.com>
2023-06-25 23:57:37 +0900

    refactor: Tag enum 데이터에 Bridge 추가

--- src/main/java/com/windry/chordplayer/spec/Tag.java
+++ src/main/java/com/windry/chordplayer/spec/Tag.java
@@ -4,11 +4,12 @@ public enum Tag {
     INTRO,
     INTERLUDE,
     MODULATION,
+    BRIDGE,
     OUTRO;
 
-    public static Tag findTagByString(String str){
-        for(Tag tag: Tag.values()){
-            if(tag.name().equalsIgnoreCase(str))
+    public static Tag findTagByString(String str) {
+        for (Tag tag : Tag.values()) {
+            if (tag.name().equalsIgnoreCase(str))
                 return tag;
         }
         return null;
