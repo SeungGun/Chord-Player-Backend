0503cad36105bbbdb09d7942c943ec1b895e7c13
SeungGun <saverrw2864@naver.com>
2023-07-05 21:13:20 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/app
-JAR_PATH=$REPOSITORY/build/libs
+cd $REPOSITORY
+
+JAR_NAME= $(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 APP_LOG=$REPOSITORY/application.log
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
 
-CURRENT_PID=$(pgrep -fl java | grep $JAR_PATH | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl java | grep $REPOSITORY/build/libs/ | awk '{print $1}')
 
-if [ -z "$CURRENT_PID" ]; then
-echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ] 
+then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-echo "> kill -15 $CURRENT_PID"
-kill -15 $CURRENT_PID
-sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> $JAR_PATH 실행"
 
-JAR_NAME=$(ls -tr $JAR_PATH/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar $JAR_NAME > $APP_LOG 2>&1 &
+nohup java -jar $JAR_PATH > $APP_LOG 2>&1 &
 
 echo "> 애플리케이션 배포 완료"
