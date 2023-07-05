de81bfac80c069d854014c985875692a6221eb4b
SeungGun <saverrw2864@naver.com>
2023-07-05 21:25:14 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,11 @@ REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
 JAR_NAME= $(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+echo "> $JAR_NAME"
+
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+echo "> $JAR_PATH"
+
 APP_LOG=$REPOSITORY/application.log
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
